eccc20cfeee18124bbd7e0b9e63c6a1ef6408f27
albortas <garlad628@gmail.com>
2025-09-07 13:38:27 -0400

    Creando vimtex sin build (carpeta auxiliar)

--- lua/plugins/lsp/latex/vimtex.lua
+++ lua/plugins/lsp/latex/vimtex.lua
@@ -20,7 +20,6 @@ return {
 					"-file-line-error",
 					"-synctex=1", -- para sincronizacion inversa
 					"-shell-escape", -- si usas minted, tikz, etc.
-					"-outdir=build", -- ✅ Así SÍ se crea/usa la carpeta build
 				},
 			}
 
